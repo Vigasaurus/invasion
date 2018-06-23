5d235cc0ebc389a6198b8024d6f301f664c8af45
Chris Ozols <chris.v.ozols@gmail.com>
2018-06-23 17:30:09 -0500

    dnd less stupid

--- src/components/DraggableSidebarBorder.js
+++ src/components/DraggableSidebarBorder.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
 import { getEmptyImage } from 'react-dnd-html5-backend';
 import cn from 'classnames';
 import { Icon } from 'antd';
@@ -10,11 +10,48 @@ const borderSource = {
 	isDragging: (props, monitor) => monitor.getItem().id === props.id,
 };
 
-const collect = (connect, monitor) => ({
+const dragCollect = (connect, monitor) => ({
 	connectDragSource: connect.dragSource(),
 	isDragging: monitor.isDragging(),
 	connectDragPreview: connect.dragPreview(),
 });
+
+const dropCollect = (connect, monitor) => ({ connectDropTarget: connect.dropTarget() });
+const spec = {
+	hover(props, monitor, component) {
+		const newWidth = window.innerWidth - monitor.getClientOffset().x;
+		console.log(newWidth);
+
+		if (newWidth <= 100) {
+			if (!component.state.isCollapsing) {
+				component.setState({
+					isCollapsing: true,
+				});
+			}
+		} else {
+			// if (newWidth > 100 && component.state.isCollapsing) {
+			// 	component.setState({ isCollapsing: false });
+			// }
+			props.updateSidebarWidth((window.innerWidth - monitor.getClientOffset().x).toString());
+		}
+	},
+	drop(props, monitor, component) {
+		const newWidth = window.innerWidth - monitor.getClientOffset().x;
+
+		props.cookies.set('sidebarWidth', newWidth, { path: '/' });
+
+		if (newWidth <= 100 && !component.state.isCollapsed) {
+			component.setState(
+				{
+					isCollapsed: true,
+				},
+				() => {
+					props.updateSidebarWidth('0');
+				}
+			);
+		}
+	},
+};
 
 export class DraggableSidebarBorder extends React.PureComponent {
 	constructor() {
@@ -34,16 +71,18 @@ export class DraggableSidebarBorder extends React.PureComponent {
 	}
 
 	render() {
-		const { isDragging, isCollapsed, connectDragSource } = this.props;
+		const { isDragging, isCollapsed, connectDragSource, connectDropTarget } = this.props;
 		const classes = cn('sidebar-border', {
 			dragging: isDragging,
 			collapsed: isCollapsed,
 		});
 
 		return connectDragSource(
-			<section onDoubleClick={this.handleDoubleClick} className={classes}>
-				{isCollapsed && <Icon type="left" />}
-			</section>
+			connectDropTarget(
+				<section onDoubleClick={this.handleDoubleClick} className={classes}>
+					{isCollapsed && <Icon type="left" />}
+				</section>
+			)
 		);
 	}
 }
@@ -53,4 +92,6 @@ DraggableSidebarBorder.propTypes = {
 	updateSidebarWidth: PropTypes.func,
 };
 
-export default DragSource('sidebar', borderSource, collect)(DraggableSidebarBorder);
+export default DropTarget('sidebar', spec, dropCollect)(
+	DragSource('sidebar', borderSource, dragCollect)(DraggableSidebarBorder)
+);
