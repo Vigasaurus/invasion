e6238901bd402b8b3e2aa2085837581032f4c685
Chris Ozols <chris.v.ozols@gmail.com>
2018-07-01 21:36:11 -0500

    close button on settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Switch } from 'antd';
+import { Row, Col, Switch, Icon } from 'antd';
+import { Link } from 'react-router-dom';
 
 export class Settings extends React.Component {
 	handleTimestampToggle = e => {
@@ -15,6 +16,9 @@ export class Settings extends React.Component {
 
 		return (
 			<section className="settings-container">
+				<Link to="/game" className="settings-close-button">
+					<Icon type="close" />
+				</Link>
 				<h1>Settings for {userInfo.username}</h1>
 				<Row>
 					<Col span={6} className="centered-item">
