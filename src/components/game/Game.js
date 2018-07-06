14dd8b53619b2f07eff47876bfad6c44d282de15
Chris Ozols <chris.v.ozols@gmail.com>
2018-07-05 19:25:35 -0500

    world map svg added

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Map from './map';
 
 export class Game extends React.Component {
 	render() {
-		return <section className="game-container">game</section>;
+		return (
+			<section className="game-container">
+				<Map />
+			</section>
+		);
 	}
 }
 
