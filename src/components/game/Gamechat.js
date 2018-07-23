e47ba61d52123ead800ec91c8409fa8d0b3767fb
Chris Ozols <chris.v.ozols@gmail.com>
2018-07-22 19:23:35 -0500

    chat form in

--- src/components/game/Gamechat.js
+++ src/components/game/Gamechat.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Switch } from 'antd';
+import { Switch, Form, Input, Button } from 'antd';
 // import PropTypes from 'prop-types';
 
 export class Gamechat extends React.Component {
 	state = {
 		chatFilter: true,
 		gameChatFilter: true,
+		chatInputValue: '',
+	};
+
+	updateState = (stateName, value) => {
+		this.setState({ [stateName]: value });
 	};
 
 	handleFilterChange = type => {
@@ -14,31 +19,63 @@ export class Gamechat extends React.Component {
 		}));
 	};
 
-	render() {
+	handleChatFormSubmit = e => {
+		e.preventDefault();
+
+		console.log('Hello, World!');
+	};
+
+	renderHeader() {
 		const { chatFilter, gameChatFilter } = this.state;
 
 		return (
+			<div className="gamechat-header">
+				<span>
+					Chat<Switch
+						onChange={() => {
+							this.handleFilterChange('chatFilter');
+						}}
+						checked={chatFilter}
+						size="small"
+					/>
+				</span>
+				<span>
+					Game<Switch
+						onChange={() => {
+							this.handleFilterChange('gameChatFilter');
+						}}
+						checked={gameChatFilter}
+						size="small"
+					/>
+				</span>
+			</div>
+		);
+	}
+
+	renderInputForm() {
+		const { chatInputValue } = this.state;
+
+		return (
+			<Form onSubmit={this.handleChatFormSubmit}>
+				<Input
+					placeholder="Chat"
+					onChange={e => {
+						this.updateState('chatInputValue', e.target.value);
+					}}
+					value={chatInputValue}
+				/>
+				<Button type="primary" onClick={this.handleChatFormSubmit} disabled={!chatInputValue}>
+					Chat
+				</Button>
+			</Form>
+		);
+	}
+
+	render() {
+		return (
 			<section className="gamechat-container">
-				<div className="gamechat-header">
-					<span>
-						Chat<Switch
-							onChange={() => {
-								this.handleFilterChange('chatFilter');
-							}}
-							checked={chatFilter}
-							size="small"
-						/>
-					</span>
-					<span>
-						Game<Switch
-							onChange={() => {
-								this.handleFilterChange('gameChatFilter');
-							}}
-							checked={gameChatFilter}
-							size="small"
-						/>
-					</span>
-				</div>
+				{this.renderHeader()}
+				{this.renderInputForm()}
 			</section>
 		);
 	}
